53cf762c6e8852f8a39cd4e846a7d1144de1d7e3
Jair <jairaceves56@gmail.com>
2023-01-31 14:19:13 -0600

    Colores agregados

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,6 +6,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primaryColor: '#3195ff',
+      secondaryColor:' #333',
+      darkColor: '#000',
+      lightColor: '#f4f4f4',
+      greenBorder: '#1CB698',
+      veryDarkBlue: 'hsl(229, 31%, 21%)',
+    },
+    fontFamily: {
+      sans: ['Barlow Semi Condensed', 'sans-serif'],
+      mono: ['Righteous'],
+    },
+    opacity: {
+      '8': '.8',
+    }
   },
   plugins: [],
 }
