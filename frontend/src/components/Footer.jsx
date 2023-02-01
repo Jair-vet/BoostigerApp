1e4ffb912cb9dc11b119f7b1ade34bc659543719
Jair <jairaceves56@gmail.com>
2023-02-01 16:44:33 -0600

    Modificamos el footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export const Footer = () => {
   return (
     <>
-        <footer class="py-12 bg-primaryColor">
+        <footer className="py-12 bg-primaryColor">
             <div
-                class="container flex flex-col items-center justify-between mx-auto space-y-16 px-6 
+                className="container flex flex-col items-center justify-between mx-auto space-y-16 px-6 
                     md:flex-row md:space-y-0">
-                <div class="flex flex-col items-center justify-between space-y-8 text-lg font-light 
+                <div className="flex flex-col items-center justify-between space-y-8 text-lg font-light 
                     md:flex-row md:space-y-0 md:space-x-14 text-grayishBlue">
                     {/* Logo */}
                     <h1 className='text-4xl text-lightColor font-mono font-normal md:absolute right-[20px]'>Boos
                         <span className='text-secondaryColor font-bold'>Tiger</span>
                     </h1>
-                    <a href="#features" class="footerEnlces font-mono">Instagram</a>
-                    <a href="#download" class="footerEnlces font-mono">WhatsApp</a>
-                    <a href="#faq" class="footerEnlces font-mono">About Us</a>
+                    <a href="#features" className="footerEnlces font-mono">Instagram</a>
+                    <a href="#download" className="footerEnlces font-mono">WhatsApp</a>
+                    <a href="#faq" className="footerEnlces font-mono">About Us</a>
                 </div>
             </div>
         </footer>
