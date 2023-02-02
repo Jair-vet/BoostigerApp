8520cdbf57656a8301d20458a213b156e33ec495
Jair <jairaceves56@gmail.com>
2023-02-01 18:21:31 -0600

    Cambiamos el nombre de los productos

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
     _id: '1',
-    name: 'Airpods Wireless Bluetooth Headphones',
+    name: 'Botas de Piel de Serpiente BossTiger',
     image: '../images/botas.png',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
@@ -14,7 +14,7 @@ const products = [
   },
   {
     _id: '2',
-    name: 'iPhone 11 Pro 256GB Memory',
+    name: 'Tenis de Piel de Serpiente con Calavera de color',
     image: '../images/tenis.png',
     description:
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
@@ -27,7 +27,7 @@ const products = [
   },
   {
     _id: '3',
-    name: 'Cannon EOS 80D DSLR Camera',
+    name: 'Zapato negro Elegante de Piel de serpiente',
     image: '../images/zapatos.png',
     description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
@@ -40,7 +40,7 @@ const products = [
   },
   {
     _id: '4',
-    name: 'Sony Playstation 4 Pro White Version',
+    name: 'Funda de Piel con llavero',
     image: '../images/funda.png',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
@@ -53,7 +53,7 @@ const products = [
   },
   {
     _id: '5',
-    name: 'Logitech G-Series Gaming Mouse',
+    name: 'Llaveros de piel diferentes colores',
     image: '../images/variosLlaveros.png',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
@@ -66,7 +66,7 @@ const products = [
   },
   {
     _id: '6',
-    name: 'Amazon Echo Dot 3rd Generation',
+    name: 'Funda de Piel Roja con llavero personalizado',
     image: '../images/fundaRoja.png',
     description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
