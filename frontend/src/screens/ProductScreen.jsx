3e163aa0423ca463952d59f9c38db407ef1da7fb
Jair <jairaceves56@gmail.com>
2023-02-03 11:19:30 -0600

    Producto Screen Estilos agregados

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -21,15 +21,14 @@ export const ProductScreen = () => {
         {/* <!-- Global Container --> */}
         <div class="flex items-center justify-center mt-12">
             {/* <!-- Card Container --> */}
-            <div class="flex flex-col p-6 m-3 space-y-10 border-2 border-dashed rounded-2xl shadow-2xl 
-                        md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16"
-            >
+            <div class="flex flex-col p-6 m-3 space-y-10 rounded-2xl shadow-2xl 
+                        md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16 bg-coverColor">
                 {/* <!-- Image Div --> */}
-                <div>
-                    <img src={`${product.image}`}
+                <div className='flex items-center'>
+                    <img src={`${product.image}`} 
                         alt="Image-product"
                         class="mx-auto duration-200 w-60 hover:scale-105 md:hover:scale-110 hover:-translate-y-4 
-                                md:hover:-translate-y-6 rounded-md"
+                                md:hover:-translate-y-6 rounded-md bg-lightColor"
                     />
                 </div>
 
@@ -39,27 +38,26 @@ export const ProductScreen = () => {
                     <div class="flex flex-col mb-4 space-y-3 text-center md:text-left">
 
                         {/* <!-- Title --> */}
-                        <div class="max-w-sm text-2xl font-mono">
+                        <div class="max-w-sm text-2xl font-mono flex justify-start">
                             {product.name}
                         </div>
 
                         {/* <!-- Price Container --> */}
                         <div class="flex flex-col mb-4 space-y-3 text-center md:text-left">
-                            <p class="line-through text-red-500">{product.price}</p>
-                            <p class="text-5xl font-bold ">{product.offert}</p>
-                            <p class="text-sm font-light text-gray-400 w-[350px]">
+                            <p class="line-through text-redColor">{product.price}</p>
+                            <p class="text-5xl font-bold">{product.offert}</p>
+                            <p class="text-sm font-medium text-grayColor w-[350px]">
                                 {product.description}
                             </p>
                         </div>
 
                         {/* <!-- Button Group --> */}
                         <div class="group">
-                            <button class="w-full transition-all duration-150 bg-blue-700 text-white border-b-8 border-b-blue-700
+                            <button class="w-full transition-all duration-150 bg-buttonsColor uppercase font-mono border-b-8 border-b-buttonColor
                                             rounded-lg group-hover:border-t-8 
-                                            group-hover:border-b-0 group-hover:bg-blue-700 group-hover:border-t-blue-700
-                                            group-hover:shadow-lg"
-                            >
-                                <div class="px-8 py-4 duration-150 bg-blue-500 rounded-lg group-hover:bg-blue-700">
+                                            group-hover:border-b-0 group-hover:bg-buttonsColor group-hover:border-t-buttonColor
+                                            group-hover:shadow-lg">
+                                <div class="px-8 py-4 duration-150 bg-buttonsColor rounded-lg group-hover:bg-buttonsColor border-t-buttonsColor">
                                     Add to cart
                                 </div>
                             </button>
@@ -67,26 +65,20 @@ export const ProductScreen = () => {
 
                         {/* <!-- Stock --> */}
                         <div class="flex items-center space-x-3 group">
-                            <div class="w-3 h-3 bg-green-400 rounded-full group-hover:animate-ping"></div>
-                            <div class="text-sm">50+ pcs. in stock</div>
+                            <div class="w-3 h-3 bg-greenColor rounded-full group-hover:animate-ping"></div>
+                            <div class="text-sm font-mono">{product.countInStock} piezas en stock</div>
                         </div>
 
                         {/* <!-- Buttoms Containers --> */}
                         <div class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
                             <button 
-                                class="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300
-                                        rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all 
-                                        duration-150"
-                            >
-                                <img src="https://raw.githubusercontent.com/Jair-vet/Producto-model-Card/main/images/weight.png" alt="" class="w-8"/>
+                                class="product-buttons hover:text-lightColor">
+                                <i className='fas fa-shopping-cart'></i>
                                 <span>Add to Cart</span>
                             </button>
                             <button 
-                                class="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300
-                                        rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all 
-                                        duration-150"
-                            >
-                                <img src="https://raw.githubusercontent.com/Jair-vet/Producto-model-Card/main/images/heart.png" alt="" class="w-8"/>
+                                class="product-buttons">
+                                <i class="fa-regular fa-heart"></i>
                                 <span>Add to Wishlist</span>
                             </button>
                         
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -13,7 +13,15 @@ module.exports = {
       lightColor: '#f4f4f4',
       greenBorder: '#1CB698',
       veryDarkBlue: 'hsl(229, 31%, 21%)',
-      yellowSpan: '#ceaa0d'
+      yellowSpan: '#ceaa0d',
+      coverColor: '#1E5F74',
+      buttonsColor: '#133B5C',
+      buttonColor: '#1D2D50',
+      greenColor: '#03C988',
+      beigeColor: '#FCDAB7',
+      redColor: '#B42B51',
+      grayColor: '#BDCDD6',
+      skyblueColor: '#6096B4',
     },
     fontFamily: {
       sans: ['Barlow Semi Condensed', 'sans-serif'],
