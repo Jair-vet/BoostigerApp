95dd34d6adec8aa26a952acfcb08474cae71dd02
Jair <jairaceves56@gmail.com>
2023-02-04 12:58:30 -0600

    Hacemos fetch desde el servidor para traer los productos

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Catalogo } from '../components/Catalogo'
 import { Product } from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 export const HomeScreen = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      const fetchProducts = async () => {
+        const { data } = await axios.get('http://127.0.0.1:5000/api/products')
+
+        setProducts(data)
+      }
+
+      fetchProducts()
+    }, [])
+    
+
   return (
     <div className='p-2 container'>
         <h1 className='text-center text-2xl uppercase font-mono'>Lastest <span>Products</span></h1>
@@ -16,20 +31,7 @@ export const HomeScreen = () => {
         </div>
 
         {/* Catalogo */}
-        <div className='flex w-full p-8 gap-2 h-[200px]'>
-            <div className='w-full text-center transition duration-500 hover:-translate-y-3'>
-                
-                <a href='#'>
-                    <h2 className='text-3xl font-mono'>Encuentra todos nuestros diseños</h2>
-                        {/* Logo */}
-                    <h1 className='text-4xl text-lightColor font-mono font-normal mt-2'>Boos
-                        <span className='text-primaryColor font-bold'>Tiger</span>
-                    </h1>
-                    <h4 className='text-sm font-thin uppercase mt-12'>Catálogo</h4>
-                    <h4 className='text-[13px] font-thin uppercase'>Elige el producto de tu agrado</h4>
-                </a>
-            </div>
-        </div>
+        <Catalogo />
     </div>
   )
 }
